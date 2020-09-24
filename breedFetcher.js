fc523546e407db2955be33ee364a4e9a8bb2325f
Michelle Tran <michelletraan@hotmail.com>
2020-09-24 23:08:20 +0000

    final commit breed fetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,21 @@
 const request = require('request');
-const breedSearch = process.argv[2];
-const URL = 'https://api.thecatapi.com/v1/breeds/search?q=';
 
-request((URL + breedSearch), (error, response, body) => {
-  if (error) {
-    console.log('error:', error);
-    process.exit();
-  }
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log('Requested breed cannot be found. Please double check!');
-  } else {
-    console.log(data[0]['description']);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+    const data = JSON.parse(body);
+    if (error) {
+      callback(error);
+    }
+    if (response.statusCode !== 200) {
+      callback(null,'HTTP Response: ${response.statusCode}');
+    }
+    if (data[0] === undefined) {
+      callback('Requested breed cannot be found. Please double check!');
+    } else if (data[0]['description']) {
+      callback(null, data[0]['description']);
+    }
+  });
+};
 
+
+module.exports = { fetchBreedDescription };
