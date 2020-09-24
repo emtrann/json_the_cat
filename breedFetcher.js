ab944f0335a0631a4453dbec09db9650a8451781
Michelle Tran <michelletraan@hotmail.com>
2020-09-24 20:45:48 +0000

    completed breedFetcher with edgecases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,17 @@
-const request = require('library');
+const request = require('request');
+const breedSearch = process.argv[2];
+const URL = 'https://api.thecatapi.com/v1/breeds/search?q=';
+
+request((URL + breedSearch), (error, response, body) => {
+  if (error) {
+    console.log('error:', error);
+    process.exit();
+  }
+  const data = JSON.parse(body);
+  if (data[0] === undefined) {
+    console.log('Requested breed cannot be found. Please double check!');
+  } else {
+    console.log(data[0]['description']);
+  }
+});
+
